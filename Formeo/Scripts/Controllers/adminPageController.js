295689e89428b24816a88c37af6df6aabf566d44
serhiivoznyi <serhiivoznyi@gmail.com>
2015-09-11 20:45:06 +0300

    Some files with js

---
+++ Formeo/Scripts/Controllers/adminPageController.js
@@ -0,0 +1,19 @@
+﻿var adminPageController = function ($scope) {
+    $scope.selectedMainMenu = '/Clients';
+    $scope.selectedCLientsMenu = '/AddUser';
+    $scope.helloVariable = 'I work!';
+    $scope.mainMenu = []
+    $scope.selectMainMenu = function (item) {
+        $scope.selectedMainMenu = item;
+    };
+    $scope.isActiveMainMenu = function (item) {
+        return $scope.selectedMainMenu === item;
+    };
+
+    $scope.selectClientMenu = function (item) {
+        $scope.selectedCLientsMenu = item;
+    };
+    $scope.isActiveCLientsMenu = function (item) {
+        return $scope.selectedCLientsMenu === item;
+    };
+}
